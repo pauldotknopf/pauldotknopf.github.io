5b27d541666ef1c1d0669b733302c0e1bdea2cee
Paul Knopf <pauldotknopf@gmail.com>
2016-03-27 20:24:45 -0400

    Added a vm to initial a jekyll virtual machine.

---
+++ Vagrantfile
@@ -0,0 +1,19 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  # Use Ubuntu 14.04 Trusty Tahr 64-bit as our operating system
+  config.vm.box = "ubuntu/trusty64"
+
+  # Configurate the virtual machine to use 2GB of RAM
+  config.vm.provider :virtualbox do |vb|
+    vb.customize ["modifyvm", :id, "--memory", "2048"]
+  end
+
+  # Forward the Jekyll server default port to the host
+  config.vm.network "forwarded_port", guest: 4000, host: 4000
+
+  config.vm.provision :shell, :path => "provision/main.sh"
+end
---
+++ provision/main.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+RUBY_VERSION=2.2.2
+RUBY_GEMS="jekyll coderay jekyll-sitemap"
+RVM_PATH=/usr/local/rvm
+
+# Update the version information
+sudo apt-get update
+
+# Install the pre-requisite packages
+sudo apt-get install build-essential nodejs git -y
+sudo apt-get autoremove -y
+
+# RVM
+if [ ! -e $RVM_PATH ]; then
+	gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+	curl -sSL https://get.rvm.io | bash -s
+fi
+
+# Ruby
+source $RVM_PATH/scripts/rvm
+
+rvm use --install $RUBY_VERSION --default
+gem install $RUBY_GEMS
+
+# Install pip and pygments
+curl https://bootstrap.pypa.io/get-pip.py | python
+pip install pygments
