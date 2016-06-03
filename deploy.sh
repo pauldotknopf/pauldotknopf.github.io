467c70c3b57924a9004fad1366a06238c38656cf
Paul Knopf <pauldotknopf@gmail.com>
2016-06-03 01:01:03 -0700

    Minor changes.

--- deploy.sh
+++ deploy.sh
@@ -37,14 +37,13 @@ cd out
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-git add .
-
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ "$(git diff --cached --exit-code)" = "" ]; then
+if [ "$(git status --porcelain)" = "" ]; then
     echo "No changes to the output on this push; exiting."
     exit 0
 fi
 
+git add .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
