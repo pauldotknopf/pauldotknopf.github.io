536e83fe91a3b5dfc79ef864206949f71e08db77
Paul Knopf <pauldotknopf@gmail.com>
2016-06-06 22:57:22 -0700

    Updating deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-SOURCE_BRANCH="master"
-TARGET_BRANCH="gh-pages"
+SOURCE_BRANCH="staging"
+TARGET_BRANCH="master"
 
 function doCompile {
   ./compile.sh
