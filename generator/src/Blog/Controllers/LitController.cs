b504f90168a017b6ea78e7670f877e5b4d16ec3c
Paul Knopf <paul.knopf@ensemblehp.com>
2023-04-07 23:10:54 -0400

    Using secret key for getting wind report.

--- generator/src/Blog/Controllers/LitController.cs
+++ generator/src/Blog/Controllers/LitController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Blog.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using Octokit;
 
 namespace Blog.Controllers
 {
@@ -13,10 +15,20 @@ namespace Blog.Controllers
 	{
 		public async Task<ActionResult> Index()
 		{
+			var apiKey = Environment.GetEnvironmentVariable("IKSBOT_KEY");
+			if (string.IsNullOrEmpty(apiKey))
+			{
+				throw new Exception("IKSBOT_KEY environment variable not set.");
+			}
 			using(var httpClient = new HttpClient())
 			{
-				var content = await httpClient.GetStringAsync("https://raw.githubusercontent.com/pauldotknopf/ikitesurf-bot/reports/report.json");
-				var model = JsonConvert.DeserializeObject<SessionReportModel>(content, new JsonSerializerSettings
+				var byteArray = Encoding.ASCII.GetBytes($"x-access-token:{apiKey}");
+				httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+
+				var httpResponseMessage = await httpClient.GetAsync("https://raw.githubusercontent.com/pauldotknopf/ikitesurf-bot/reports/report.json").ConfigureAwait(false);
+				var response = await httpResponseMessage.Content.ReadAsStringAsync().ConfigureAwait(false);
+                
+				var model = JsonConvert.DeserializeObject<SessionReportModel>(response, new JsonSerializerSettings
                 {
                     ContractResolver = new CamelCasePropertyNamesContractResolver()
                 });
